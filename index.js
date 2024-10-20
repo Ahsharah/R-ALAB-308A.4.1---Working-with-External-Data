31069205025bf2f5471472761fa31108dd3bbbbc
Alexandria <walkerciara24@gmail.com>
2024-10-20 06:29:54 +0100

    Importing templet from CodeSandbox and starting index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import * as Carousel from "./Carousel.js";
+import axios from "axios";
+
+// DOM Elements
+const breedSelect = document.getElementById("breedSelect");
+const infoDump = document.getElementById("infoDump");
+const progressBar = document.getElementById("progressBar");
+const getFavouritesBtn = document.getElementById("getFavouritesBtn");
